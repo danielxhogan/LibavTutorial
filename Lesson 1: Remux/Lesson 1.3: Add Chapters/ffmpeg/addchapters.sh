e401944bf68ef6054ff1f1cd02ee285606eb0d2e
danielxhogan <danielxhogan@gmail.com>
2025-06-08 17:41:27 -0700

    add Lesson 1.3: Add Chapters

---
+++ Lesson 1: Remux/Lesson 1.3: Add Chapters/ffmpeg/addchapters.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+source ~/.zshrc
+
+# ffmpeg \
+#   -i "./videos/inputs/h4m_ikkf.flv" \
+#   -i "chapters.txt" \
+#   -map_metadata 1 \
+#   -c copy \
+#   -metadata title="h4m_ikkf.flv" "./videos/ffmpeg/h4m_ikkf.flv"
+
+# ffmpeg \
+#   -i "./videos/inputs/tv_ikkf.ogg" \
+#   -i "chapters.txt" \
+#   -map_metadata 1 \
+#   -c copy \
+#   -metadata title="tv_ikkf.ogg" "./videos/ffmpeg/tv_ikkf.ogg"
---
+++ Lesson 1: Remux/Lesson 1.3: Add Chapters/run.sh
@@ -0,0 +1,16 @@
+# cd build && ninja && ./addchapters "../videos/inputs/av1_mp3.mp4" "../chapters.txt" "../videos/outputs/av1_mp3.mp4" "av1_mp3.mp4"
+# cd build && ninja && ./addchapters "../videos/inputs/h264_eac3.mkv" "../chapters.txt" "../videos/outputs/h264_eac3.mkv" "h264_eac3.mkv"
+# cd build && ninja && ./addchapters "../videos/inputs/vp9_opus.webm" "../chapters.txt" "../videos/outputs/vp9_opus.webm" "vp9_opus.webm"
+# cd build && ninja && ./addchapters "../videos/inputs/h264_mp3.mov" "../chapters.txt" "../videos/outputs/h264_mp3.mov" "h264_mp3.mov"
+# cd build && ninja && ./addchapters "../videos/inputs/h264_mp3.wmv" "../chapters.txt" "../videos/outputs/h264_mp3.wmv" "h264_mp3.wmv"
+
+# supports chapters as cue points
+# cd build && ninja && ./addchapters "../videos/inputs/h264_mp3.flv" "../chapters.txt" "../videos/outputs/h264_mp3.flv" "h264_mp3.flv"
+
+# supports chapters as comments
+# cd build && ninja && ./addchapters "../videos/inputs/theora_vorbis.ogg" "../chapters.txt" "../videos/outputs/theora_vorbis.ogg" "theora_vorbis.ogg"
+
+# doesn't work. don't know why
+# cd build && ninja && ./addchapters "../videos/inputs/h264_mp3.avi" "../chapters.txt" "../videos/outputs/h264_mp3.avi" "h264_mp3.avi"
+
+# cd build && ninja && ./addchapters
