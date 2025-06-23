7f253a15f79aca47cd06d935db10143b089b64a3
danielxhogan <danielxhogan@gmail.com>
2025-06-23 12:14:26 -0700

    add test videos. update run commands

--- Lesson 1: Remux/Lesson 1.7: Clipping/run.sh
+++ Lesson 1: Remux/Lesson 1.7: Clipping/run.sh
@@ -1,3 +1,5 @@
-cd build && ninja && ./clip 4 "../videos/inputs/h264_eac3.mkv" 15 "../videos/outputs/h264_eac3.mkv"
-# cd build && ninja && ./clip 2 "../videos/inputs/av1_mp3.mp4" 6 "../videos/outputs/av1_mp3.mp4"
+# cd build && ninja && ./clip 1 "../videos/inputs/vp9_opus.webm" 3 "../videos/outputs/vp9_opus.webm"
+# cd build && ninja && ./clip 5 "../videos/inputs/av1_mp3.mp4" 5 "../videos/outputs/av1_mp3.mp4"
+# cd build && ninja && ./clip 2 "../videos/inputs/ikkf.mkv" 5 "../videos/outputs/ikkf.mkv"
+
 # cd build && ninja && ./clip
