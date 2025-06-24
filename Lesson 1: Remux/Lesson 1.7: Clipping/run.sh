4a7986b0450684444ac60d742c53a90deba1fdee
danielxhogan <danielxhogan@gmail.com>
2025-06-24 11:21:29 -0700

    add videos

--- Lesson 1: Remux/Lesson 1.7: Clipping/run.sh
+++ Lesson 1: Remux/Lesson 1.7: Clipping/run.sh
@@ -1,6 +1,8 @@
-# cd build && ninja && ./clip 10 "../videos/inputs/ig.mkv" 5 "../videos/outputs/ig.mkv"
+# cd build && ninja && ./clip 0 "../videos/inputs/ig.mkv" 10 "../videos/outputs/ig.mkv"
 # cd build && ninja && ./clip 2 "../videos/inputs/ikkf.mkv" 5 "../videos/outputs/ikkf.mkv"
 # cd build && ninja && ./clip 1 "../videos/inputs/vp9_opus.webm" 6 "../videos/outputs/vp9_opus.webm"
-# cd build && ninja && ./clip 3 "../videos/inputs/h264_eac3.mkv" 3 "../videos/outputs/h264_eac3.mkv"
+# cd build && ninja && ./clip 3 "../videos/inputs/h264_eac3.mkv" 8 "../videos/outputs/h264_eac3.mkv"
+# cd build && ninja && ./clip 6 "../videos/inputs/av1_mp3.mp4" 3 "../videos/outputs/av1_mp3.mp4"
+# cd build && ninja && ./clip 6 "../videos/inputs/h264_mp3.flv" 4 "../videos/outputs/h264_mp3.flv"
 
 # cd build && ninja && ./clip
