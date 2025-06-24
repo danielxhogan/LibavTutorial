e6db744d367fc15d0d95fd7856e8b01d157db061
danielxhogan <danielxhogan@gmail.com>
2025-06-23 23:23:34 -0700

    update videos

--- Lesson 1: Remux/Lesson 1.7: Clipping/run.sh
+++ Lesson 1: Remux/Lesson 1.7: Clipping/run.sh
@@ -1,5 +1,6 @@
-# cd build && ninja && ./clip 1 "../videos/inputs/vp9_opus.webm" 3 "../videos/outputs/vp9_opus.webm"
-# cd build && ninja && ./clip 5 "../videos/inputs/av1_mp3.mp4" 5 "../videos/outputs/av1_mp3.mp4"
+# cd build && ninja && ./clip 10 "../videos/inputs/ig.mkv" 5 "../videos/outputs/ig.mkv"
 # cd build && ninja && ./clip 2 "../videos/inputs/ikkf.mkv" 5 "../videos/outputs/ikkf.mkv"
+# cd build && ninja && ./clip 1 "../videos/inputs/vp9_opus.webm" 6 "../videos/outputs/vp9_opus.webm"
+# cd build && ninja && ./clip 3 "../videos/inputs/h264_eac3.mkv" 3 "../videos/outputs/h264_eac3.mkv"
 
 # cd build && ninja && ./clip
