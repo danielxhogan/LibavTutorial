ba6f7513decd12894c2652451a1a67d587999269
danielxhogan <danielxhogan@gmail.com>
2025-12-02 00:09:11 -0800

    move main transcode loop to seperate function

--- Lesson 2: Transcode/Lesson 2.1: Transcode Video/vtranscode.c
+++ Lesson 2: Transcode/Lesson 2.1: Transcode Video/vtranscode.c
@@ -277,6 +277,58 @@ void close_output(OutputContext *out_ctx)
   free(out_ctx);
 }
 
+int transcode(InputContext *in_ctx, AVStream *in_stream,
+  OutputContext *out_ctx, AVStream *out_stream)
+{
+  int ret = 0;
+
+  while ((ret = av_read_frame(in_ctx->fmt_ctx, in_ctx->pkt)) >= 0)
+  {
+    if (!(in_ctx->pkt->stream_index == in_ctx->stream_idx)) {
+      av_packet_unref(in_ctx->pkt);
+      continue;
+    }
+
+    if ((ret = avcodec_send_packet(in_ctx->dec_ctx, in_ctx->pkt)) < 0) {
+      fprintf(stderr, "Failed to send packet to decoder.\n");
+      return ret;
+    }
+
+    while ((ret = avcodec_receive_frame(in_ctx->dec_ctx, in_ctx->dec_frame)) >= 0)
+    {
+      if ((ret = avcodec_send_frame(out_ctx->enc_ctx, in_ctx->dec_frame)) < 0) {
+        fprintf(stderr, "Failed to send frame to encoder.\n");
+        return ret;
+      }
+
+      while ((ret =
+        avcodec_receive_packet(out_ctx->enc_ctx, out_ctx->enc_pkt)) >= 0)
+      {
+        out_ctx->enc_pkt->stream_index = 0;
+        av_packet_rescale_ts(out_ctx->enc_pkt,
+          in_stream->time_base, out_stream->time_base);
+
+        if ((ret =
+          av_interleaved_write_frame(out_ctx->fmt_ctx, out_ctx->enc_pkt)) < 0)
+        {
+          fprintf(stderr, "Failed to write packet to file.\n");
+          return ret;
+        }
+      }
+
+      if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
+        fprintf(stderr, "Failed to receive packet from encoder.\n");
+        return ret;
+      }
+    }
+
+    if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
+      fprintf(stderr, "Failed to receive frame from decoder.\n");
+      return ret;
+    }
+  }
+}
+
 int main(int argc, char **argv)
 {
   int ret = 0;
