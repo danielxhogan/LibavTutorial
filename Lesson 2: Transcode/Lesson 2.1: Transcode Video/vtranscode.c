51bd58d112f5ded5f7be504272ecbe36e503b213
danielxhogan <danielxhogan@gmail.com>
2025-12-02 10:38:49 -0800

    call flush_encoder function in main function

--- Lesson 2: Transcode/Lesson 2.1: Transcode Video/vtranscode.c
+++ Lesson 2: Transcode/Lesson 2.1: Transcode Video/vtranscode.c
@@ -478,36 +478,10 @@ int main(int argc, char **argv)
     goto end;
   }
 
-  // flush encoder
-
-  if ((ret = avcodec_send_frame(out_ctx->enc_ctx, NULL)) < 0)
-  {
-    fprintf(stderr, "Failed to send frame to encoder.\n");
-    return ret;
+  if ((ret = flush_encoder(out_ctx, out_stream, in_stream)) < 0) {
+    fprintf(stderr, "Failed to flush encoder\n");
+    goto end;
   }
-
-  while ((ret =
-    avcodec_receive_packet(out_ctx->enc_ctx, out_ctx->enc_pkt)) >= 0)
-  {
-    out_ctx->enc_pkt->stream_index = 0;
-
-    av_packet_rescale_ts(out_ctx->enc_pkt,
-      in_stream->time_base, out_stream->time_base);
-
-    if ((ret =
-      av_interleaved_write_frame(out_ctx->fmt_ctx, out_ctx->enc_pkt)) < 0)
-    {
-      fprintf(stderr, "Failed to write packet to file.\n");
-      goto end;
-    }
-  }
-
-  if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
-    fprintf(stderr, "Failed to receive packet from encoder.\n");
-    return ret;
-  }
-
-  // done flushing
 
   if ((ret = av_write_trailer(out_ctx->fmt_ctx)) < 0) {
     fprintf(stderr, "Failed to write trailer to file.\n");
