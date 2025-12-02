546d2bbf222f4a5f119788c1a58be5c72c825a0c
danielxhogan <danielxhogan@gmail.com>
2025-12-02 01:14:33 -0800

    fix flush_decoder

--- Lesson 2: Transcode/Lesson 2.1: Transcode Video/vtranscode.c
+++ Lesson 2: Transcode/Lesson 2.1: Transcode Video/vtranscode.c
@@ -337,35 +337,47 @@ int transcode(InputContext *in_ctx, AVStream *in_stream,
   return 0;
 }
 
-int flush_decoder(OutputContext *out_ctx, AVStream *out_stream,
-  AVStream *in_stream)
+int flush_decoder(InputContext *in_ctx, AVStream *in_stream,
+  OutputContext *out_ctx, AVStream *out_stream)
 {
   int ret = 0;
 
-  if ((ret = avcodec_send_frame(out_ctx->enc_ctx, NULL)) < 0)
-  {
-    fprintf(stderr, "Failed to send frame to encoder.\n");
+  if ((ret = avcodec_send_packet(in_ctx->dec_ctx, NULL)) < 0) {
+    fprintf(stderr, "Failed to send packet to decoder.\n");
     return ret;
   }
 
-  while ((ret =
-    avcodec_receive_packet(out_ctx->enc_ctx, out_ctx->enc_pkt)) >= 0)
+  while ((ret = avcodec_receive_frame(in_ctx->dec_ctx, in_ctx->dec_frame)) >= 0)
   {
-    out_ctx->enc_pkt->stream_index = 0;
+    if ((ret = avcodec_send_frame(out_ctx->enc_ctx, in_ctx->dec_frame)) < 0) {
+      fprintf(stderr, "Failed to send frame to encoder.\n");
+      return ret;
+    }
 
-    av_packet_rescale_ts(out_ctx->enc_pkt,
-      in_stream->time_base, out_stream->time_base);
-
-    if ((ret =
-      av_interleaved_write_frame(out_ctx->fmt_ctx, out_ctx->enc_pkt)) < 0)
+    while ((ret =
+      avcodec_receive_packet(out_ctx->enc_ctx, out_ctx->enc_pkt)) >= 0)
     {
-      fprintf(stderr, "Failed to write packet to file.\n");
+      out_ctx->enc_pkt->stream_index = 0;
+
+      av_packet_rescale_ts(out_ctx->enc_pkt,
+        in_stream->time_base, out_stream->time_base);
+
+      if ((ret =
+        av_interleaved_write_frame(out_ctx->fmt_ctx, out_ctx->enc_pkt)) < 0)
+      {
+        fprintf(stderr, "Failed to write packet to file.\n");
+        return ret;
+      }
+    }
+
+    if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
+      fprintf(stderr, "Failed to receive packet from encoder.\n");
       return ret;
     }
   }
 
   if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
-    fprintf(stderr, "Failed to receive packet from encoder.\n");
+    fprintf(stderr, "Failed to receive frame from decoder.\n");
     return ret;
   }
 
