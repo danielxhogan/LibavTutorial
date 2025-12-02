2ebc69c2dc7bd9aa4912b3d0e0d15c0c9803306e
danielxhogan <danielxhogan@gmail.com>
2025-12-02 11:51:03 -0800

    add arguments for width, height, and pix_fmt

--- Lesson 2: Transcode/Lesson 2.2: Rescale Video/run.sh
+++ Lesson 2: Transcode/Lesson 2.2: Rescale Video/run.sh
@@ -1,4 +1,4 @@
-# cd build && ninja && ./swstranscode "../videos/inputs/ll.mkv" "../videos/outputs/ll.mkv" "h263"
-# cd build && ninja && ./swstranscode "../videos/inputs/4k_ll.mkv" "../videos/outputs/4k_ll.mkv" "h263"
+# cd build && ninja && ./swstranscode "../videos/inputs/4k_ll.mkv" "../videos/outputs/ll_x265.mkv" "1280" "720" "yuv420p10" "libx265"
+# cd build && ninja && ./swstranscode "../videos/inputs/4k_ll.mkv" "../videos/outputs/4k_ll_x265.mkv" "1920" "1080" "yuv420p10le" "libx265"
 
 cd build && ninja && ./swstranscode 
