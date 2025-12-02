f2e537042e4934a39a8b69f4a9a9e6098ac64935
danielxhogan <danielxhogan@gmail.com>
2025-12-02 10:20:42 -0800

    call transcode function in main function

--- Lesson 2: Transcode/Lesson 2.1: Transcode Video/vtranscode.c
+++ Lesson 2: Transcode/Lesson 2.1: Transcode Video/vtranscode.c
@@ -468,55 +468,8 @@ int main(int argc, char **argv)
 
   out_stream = out_ctx->fmt_ctx->streams[0];
 
-  while ((ret = av_read_frame(in_ctx->fmt_ctx, in_ctx->pkt)) >= 0)
-  {
-    if (!(in_ctx->pkt->stream_index == in_ctx->stream_idx)) {
-      av_packet_unref(in_ctx->pkt);
-      continue;
-    }
-
-    if ((ret = avcodec_send_packet(in_ctx->dec_ctx, in_ctx->pkt)) < 0) {
-      fprintf(stderr, "Failed to send packet to decoder.\n");
-      goto end;
-    }
-
-    while ((ret = avcodec_receive_frame(in_ctx->dec_ctx, in_ctx->dec_frame)) >= 0)
-    {
-      if ((ret = avcodec_send_frame(out_ctx->enc_ctx, in_ctx->dec_frame)) < 0) {
-        fprintf(stderr, "Failed to send frame to encoder.\n");
-        goto end;
-      }
-
-      while ((ret =
-        avcodec_receive_packet(out_ctx->enc_ctx, out_ctx->enc_pkt)) >= 0)
-      {
-        out_ctx->enc_pkt->stream_index = 0;
-
-        av_packet_rescale_ts(out_ctx->enc_pkt,
-          in_stream->time_base, out_stream->time_base);
-
-        if ((ret =
-          av_interleaved_write_frame(out_ctx->fmt_ctx, out_ctx->enc_pkt)) < 0)
-        {
-          fprintf(stderr, "Failed to write packet to file.\n");
-          goto end;
-        }
-      }
-
-      if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
-        fprintf(stderr, "Failed to receive packet from encoder.\n");
-        goto end;
-      }
-    }
-
-    if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
-      fprintf(stderr, "Failed to receive frame from decoder.\n");
-      goto end;
-    }
-  }
-
-  if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
-    fprintf(stderr, "Failed to read frame from input file.\n");
+  if ((ret = transcode(in_ctx, in_stream, out_ctx, out_stream)) < 0) {
+    fprintf(stderr, "Failed to transcode\n");
     goto end;
   }
 
