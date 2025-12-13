8bdec6794698048c580b12fcc1b6230f6a6f514a
danielxhogan <danielxhogan@gmail.com>
2025-12-13 14:39:10 -0800

    add tests for x265 and mp4

--- Lesson 2: Transcode/Lesson 2.3: Transcode HDR Video/run.sh
+++ Lesson 2: Transcode/Lesson 2.3: Transcode HDR Video/run.sh
@@ -1,4 +1,13 @@
-# cd build && ninja && ./hdrtranscode "../videos/inputs/her_dovi.mkv" "../videos/outputs/her_dovi.mkv" "libsvtav1"
-# cd build && ninja && ./hdrtranscode "../videos/inputs/ll_hdr.mkv" "../videos/outputs/ll_hdr.mkv" "libsvtav1" "preset=9:crf=45"
+# cd build && ninja && ./hdrtranscode "../videos/inputs/her_dovi.mkv" "../videos/outputs/her_dovi_svtav1.mkv" "libsvtav1"
+# cd ..
+# cd build && ninja && ./hdrtranscode "../videos/inputs/her_dovi.mkv" "../videos/outputs/her_dovi_x265.mkv" "libx265"
+# cd ..
+# cd build && ninja && ./hdrtranscode "../videos/inputs/her_dovi.mkv" "../videos/outputs/her_dovi_x265.mp4" "libx265"
+# cd ..
+# cd build && ninja && ./hdrtranscode "../videos/inputs/her_dovi.mkv" "../videos/outputs/her_dovi_x265_params.mkv" "libx265" "preset=ultrafast:crf=45"
+# cd ..
+# cd build && ninja && ./hdrtranscode "../videos/inputs/ll_hdr.mkv" "../videos/outputs/ll_hdr_svtav1.mkv" "libsvtav1" "preset=9:crf=45"
+# cd ..
+# cd build && ninja && ./hdrtranscode "../videos/inputs/ll_hdr.mkv" "../videos/outputs/ll_hdr_x265.mkv" "libx265" "preset=ultrafast:crf=45"
 
 cd build && ninja && ./hdrtranscode 
