8bdba351d6a08a6ed3d071964496259c52fb84ec
danielxhogan <danielxhogan@gmail.com>
2025-12-13 13:16:26 -0800

    refactor

--- Lesson 2: Transcode/Lesson 2.3: Transcode HDR Video/hdrtranscode.c
+++ Lesson 2: Transcode/Lesson 2.3: Transcode HDR Video/hdrtranscode.c
@@ -194,6 +194,106 @@ int inject_hdr_metadta(HdrMetadataContext *hdr_ctx, AVCodecContext *enc_ctx)
   return 0;
 }
 
+typedef struct InputContext {
+  AVFormatContext *fmt_ctx;
+  AVCodecContext *dec_ctx;
+  AVFrame *dec_frame;
+  AVPacket *init_pkt;
+  int stream_idx;
+} InputContext;
+
+InputContext *open_input(const char *in_filename, unsigned int stream_idx)
+{
+  int ret = 0;
+  InputContext *in_ctx = NULL;
+  AVStream *in_stream;
+  const AVCodec *dec;
+
+  if (!(in_ctx = malloc(sizeof(InputContext)))) {
+    fprintf(stderr, "Failed to allocate InputContext.\n");
+    ret = AVERROR(ENOMEM);
+    return NULL;
+  }
+
+  in_ctx->fmt_ctx = NULL;
+  in_ctx->dec_ctx = NULL;
+  in_ctx->dec_frame = NULL;
+  in_ctx->init_pkt = NULL;
+  in_ctx->stream_idx = stream_idx;
+
+  if ((ret =
+    avformat_open_input(&in_ctx->fmt_ctx, in_filename, NULL, NULL)) < 0)
+  {
+    fprintf(stderr, "Failed to open AVFormatContext.\n");
+    return NULL;
+  }
+
+  if ((ret = avformat_find_stream_info(in_ctx->fmt_ctx, NULL)) < 0) {
+    fprintf(stderr, "Failed to find stream info.\n");
+    return NULL;
+  }
+
+  if (stream_idx >= in_ctx->fmt_ctx->nb_streams) {
+    fprintf(stderr, "Invalid stream index.\n");
+    ret = -1;
+    return NULL;
+  }
+
+  in_stream = in_ctx->fmt_ctx->streams[stream_idx];
+
+  if (!(dec = avcodec_find_decoder(in_stream->codecpar->codec_id))) {
+    fprintf(stderr, "Failed to find decoder for stream: '%d'.\n", stream_idx);
+    ret = AVERROR(EINVAL);
+    return NULL;
+  }
+
+  if (!(in_ctx->dec_ctx = avcodec_alloc_context3(dec))) {
+    fprintf(stderr,
+      "Failed to allocate decoder for stream: '%d'.\n", stream_idx);
+    ret = AVERROR(EINVAL);
+    return NULL;
+  }
+
+  if ((ret =
+    avcodec_parameters_to_context(in_ctx->dec_ctx, in_stream->codecpar)) < 0)
+  {
+    fprintf(stderr,
+      "Failed to copy parameters from input stream: '%d' to decoder.\n",
+      stream_idx);
+    return NULL;
+  }
+
+  if ((ret = avcodec_open2(in_ctx->dec_ctx, dec, NULL)) < 0) {
+    fprintf(stderr, "Failed to open decoder for stream: '%d'.\n",
+      stream_idx);
+    return NULL;
+  }
+
+  if (!(in_ctx->dec_frame = av_frame_alloc())) {
+    fprintf(stderr, "Failed to allocate AVFrame.\n");
+    ret = AVERROR(ENOMEM);
+    return NULL;
+  }
+
+  if (!(in_ctx->init_pkt = av_packet_alloc())) {
+    fprintf(stderr, "Failed to allocate AVPacket.\n");
+    ret = AVERROR(ENOMEM);
+    return NULL;
+  }
+
+  return in_ctx;
+}
+
+void close_input(InputContext *in_ctx)
+{
+  if (!in_ctx) return;
+  avformat_close_input(&in_ctx->fmt_ctx);
+  avcodec_free_context(&in_ctx->dec_ctx);
+  av_frame_free(&in_ctx->dec_frame);
+  av_packet_free(&in_ctx->init_pkt);
+  free(in_ctx);
+}
+
 int initialize_encoder_params(const char *encoder, char **enc_params_opt)
 {
   if (strcmp(encoder, "libx264") == 0) {
@@ -212,18 +312,249 @@ int initialize_encoder_params(const char *encoder, char **enc_params_opt)
   return 0;
 }
 
+typedef struct OutputContext {
+  AVFormatContext *fmt_ctx;
+  AVCodecContext *enc_ctx;
+  AVPacket *enc_pkt;
+} OutputContext;
+
+OutputContext *open_output(InputContext *in_ctx, HdrMetadataContext *hdr_ctx,
+  const char *codec, char *enc_params, char *enc_params_opt,
+  const char *out_filename)
+{
+  int ret = 0;
+  OutputContext *out_ctx = NULL;
+  AVStream *in_stream, *out_stream;
+  const AVCodec *enc;
+
+  if (!(out_ctx = malloc(sizeof(OutputContext)))) {
+    fprintf(stderr, "Failed to allocate OutputContext.\n");
+    ret = AVERROR(ENOMEM);
+    return NULL;
+  }
+
+  out_ctx->fmt_ctx = NULL;
+  out_ctx->enc_ctx = NULL;
+  out_ctx->enc_pkt = NULL;
+
+  if (!(enc = avcodec_find_encoder_by_name(codec))) {
+    fprintf(stderr, "Failed to find encoder.\n");
+    ret = AVERROR(EINVAL);
+    return NULL;
+  }
+
+  if (!(out_ctx->enc_ctx = avcodec_alloc_context3(enc))) {
+    fprintf(stderr, "Failed to allocate encoder.\n");
+    ret = AVERROR(EINVAL);
+    return NULL;
+  }
+
+  in_stream = in_ctx->fmt_ctx->streams[in_ctx->stream_idx];
+
+  out_ctx->enc_ctx->time_base = in_stream->time_base;
+  out_ctx->enc_ctx->framerate = in_stream->avg_frame_rate;
+
+  out_ctx->enc_ctx->width = in_stream->codecpar->width;
+  out_ctx->enc_ctx->height = in_stream->codecpar->height;
+  out_ctx->enc_ctx->pix_fmt = in_stream->codecpar->format;
+
+  out_ctx->enc_ctx->color_primaries = in_stream->codecpar->color_primaries;
+  out_ctx->enc_ctx->color_trc = in_stream->codecpar->color_trc;
+  out_ctx->enc_ctx->colorspace = in_stream->codecpar->color_space;
+  out_ctx->enc_ctx->color_range = in_stream->codecpar->color_range;
+  out_ctx->enc_ctx->chroma_sample_location = in_stream->codecpar->chroma_location;
+
+  out_ctx->enc_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+
+  if (enc_params && enc_params_opt) {
+    if ((ret = av_opt_set(out_ctx->enc_ctx->priv_data,
+      enc_params_opt, enc_params, 0)) < 0)
+    {
+      fprintf(stderr, "Failed to set '%s'.\n", enc_params_opt);
+      return NULL;
+    }
+  }
+
+  if ((ret = inject_hdr_metadta(hdr_ctx, out_ctx->enc_ctx)) < 0) {
+    fprintf(stderr, "Failed to inject hdr metadata.\n");
+    return NULL;
+  }
+
+  if ((ret = avcodec_open2(out_ctx->enc_ctx, enc, NULL)) < 0) {
+    fprintf(stderr, "Failed to open encoder.\n");
+    return NULL;
+  }
+
+  if ((ret =
+    avformat_alloc_output_context2(&out_ctx->fmt_ctx, NULL, NULL, out_filename)))
+  {
+    fprintf(stderr,
+      "Failed to allocate output format context.\n");
+    return NULL;
+  }
+
+  if ((ret = av_dict_copy(&out_ctx->fmt_ctx->metadata, in_ctx->fmt_ctx->metadata,
+    AV_DICT_DONT_OVERWRITE)) < 0)
+  {
+    fprintf(stderr,
+      "Failed to copy input metadata to output.\n");
+    return NULL;
+  }
+
+  if (!(out_stream = avformat_new_stream(out_ctx->fmt_ctx, NULL))) {
+    fprintf(stderr,
+      "Failed to allocate new output stream.\n");
+    return NULL;
+  }
+
+  if ((ret = av_dict_copy(&out_stream->metadata,
+    in_stream->metadata, AV_DICT_DONT_OVERWRITE)))
+  {
+    fprintf(stderr,
+      "Failed to copy metadata from input stream to output stream.\n");
+    return NULL;
+  }
+
+  if ((ret =
+    avcodec_parameters_from_context(out_stream->codecpar, out_ctx->enc_ctx)))
+  {
+    fprintf(stderr,
+      "Failed to copy parameters from encoder to output stream.\n");
+    return NULL;
+  }
+
+  out_stream->time_base = out_ctx->enc_ctx->time_base;
+  out_stream->r_frame_rate = in_stream->r_frame_rate;
+  out_stream->avg_frame_rate = in_stream->avg_frame_rate;
+
+  if (!(out_ctx->enc_pkt = av_packet_alloc())) {
+    fprintf(stderr, "Failed to allocate AVPacket.\n");
+    ret = AVERROR(ENOMEM);
+    return NULL;
+  }
+
+  if (!(out_ctx->fmt_ctx->oformat->flags & AVFMT_NOFILE)) {
+    if ((ret =
+      avio_open(&out_ctx->fmt_ctx->pb, out_filename, AVIO_FLAG_WRITE)) < 0)
+    {
+      fprintf(stderr, "Failed to create output file.\n");
+      return NULL;
+    }
+  }
+
+  if ((ret = avformat_write_header(out_ctx->fmt_ctx, NULL)) < 0) {
+    fprintf(stderr, "Failed to write header to output.\n");
+    return NULL;
+  }
+
+  return out_ctx;
+}
+
+void close_output(OutputContext *out_ctx)
+{
+  if (!out_ctx) return;
+  if (out_ctx->fmt_ctx && !(out_ctx->fmt_ctx->flags & AVFMT_NOFILE))
+    avio_closep(&out_ctx->fmt_ctx->pb);
+  avformat_free_context(out_ctx->fmt_ctx);
+  avcodec_free_context(&out_ctx->enc_ctx);
+  free(out_ctx);
+}
+
+int encode_frame(InputContext *in_ctx, AVStream *in_stream,
+  OutputContext *out_ctx, AVStream *out_stream)
+{
+  int ret = 0;
+
+  if ((ret = avcodec_send_frame(out_ctx->enc_ctx, in_ctx->dec_frame)) < 0) {
+    fprintf(stderr, "Failed to send frame to encoder.\n");
+    return ret;
+  }
+
+  while ((ret =
+    avcodec_receive_packet(out_ctx->enc_ctx, out_ctx->enc_pkt)) >= 0)
+  {
+    out_ctx->enc_pkt->stream_index = 0;
+
+    av_packet_rescale_ts(out_ctx->enc_pkt,
+      in_stream->time_base, out_stream->time_base);
+
+    if ((ret =
+      av_interleaved_write_frame(out_ctx->fmt_ctx, out_ctx->enc_pkt)) < 0)
+    {
+      fprintf(stderr, "Failed to write packet to file.\n");
+      return 0;
+    }
+  }
+
+  if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
+    fprintf(stderr, "Failed to receive packet from encoder.\n");
+    return ret;
+  }
+
+  return 0;
+}
+
+int decode_packet(InputContext *in_ctx, AVStream *in_stream,
+  OutputContext *out_ctx, AVStream *out_stream)
+{
+  int ret = 0;
+
+  if ((ret = avcodec_send_packet(in_ctx->dec_ctx, in_ctx->init_pkt)) < 0) {
+    fprintf(stderr, "Failed to send packet to decoder.\n");
+    return ret;
+  }
+
+  while ((ret = avcodec_receive_frame(in_ctx->dec_ctx, in_ctx->dec_frame)) >= 0)
+  {
+    if ((ret = encode_frame(in_ctx, in_stream, out_ctx, out_stream)) < 0) {
+      fprintf(stderr, "Failed to encode frame.\n");
+      return ret;
+    }
+  }
+
+  if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
+    fprintf(stderr, "Failed to receive frame from decoder.\n");
+    return ret;
+  }
+
+  return 0;
+}
+
+int transcode(InputContext *in_ctx, AVStream *in_stream,
+  OutputContext *out_ctx, AVStream *out_stream)
+{
+  int ret = 0;
+
+  while ((ret = av_read_frame(in_ctx->fmt_ctx, in_ctx->init_pkt)) >= 0)
+  {
+    if (!(in_ctx->init_pkt->stream_index == in_ctx->stream_idx)) {
+      av_packet_unref(in_ctx->init_pkt);
+      continue;
+    }
+
+    if ((ret = decode_packet(in_ctx, in_stream, out_ctx, out_stream)) < 0) {
+      fprintf(stderr, "Failed to decode packet.\n");
+      return ret;
+    }
+  }
+
+  if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
+    fprintf(stderr, "Failed to read frame from input file.\n");
+    return ret;
+  }
+
+  return 0;
+}
+
 int main(int argc, char **argv)
 {
+  int ret = 0;
   char *in_filename, *out_filename, *encoder,
     *enc_params = NULL, *enc_params_opt = NULL;
-  int ret = 0, v_stream_idx = -1;
+  InputContext *in_ctx = NULL;
+  OutputContext *out_ctx = NULL;
   HdrMetadataContext *hdr_ctx = NULL;
-  AVFormatContext *in_fmt_ctx = NULL, *out_fmt_ctx = NULL;
   AVStream *in_stream = NULL, *out_stream = NULL;
-  const AVCodec *dec, *enc;
-  AVCodecContext *dec_ctx = NULL, *enc_ctx = NULL;
-  AVPacket *pkt = NULL;
-  AVFrame *frame = NULL;
 
   if (argc != 4 && argc != 5) {
     printf("\nUsage: %s <input file> <output file> <encoder> [<encoder-params>]\n\n\t"
@@ -261,224 +592,48 @@ int main(int argc, char **argv)
     goto end;
   }
 
-  if ((ret = avformat_open_input(&in_fmt_ctx, in_filename, NULL, NULL)) < 0) {
-    fprintf(stderr, "Failed to open input video file: '%s'.\n", in_filename);
+  if (!(in_ctx = open_input(in_filename, 0))) {
+    fprintf(stderr, "Failed to open input file: '%s'.\n", in_filename);
     goto end;
   }
 
-  if ((ret = avformat_find_stream_info(in_fmt_ctx, NULL)) < 0) {
-    fprintf(stderr, "Failed to retrieve input stream info.");
-    goto end;
-  }
+  in_stream = in_ctx->fmt_ctx->streams[in_ctx->stream_idx];
 
-  if ((ret = v_stream_idx =
-    av_find_best_stream(in_fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0)) < 0)
+  if (!(out_ctx =
+    open_output(in_ctx, hdr_ctx, encoder, enc_params, enc_params_opt, out_filename)))
   {
-    fprintf(stderr, "Failed to find video stream in input file.\n");
-    goto end;
-  }
-  in_stream = in_fmt_ctx->streams[v_stream_idx];
-
-  if (!(dec = avcodec_find_decoder(in_stream->codecpar->codec_id))) {
-    fprintf(stderr, "Failed to find decoder.\n");
-    ret = AVERROR(EINVAL);
+    fprintf(stderr, "Failed to open output file: '%s'.\n", out_filename);
     goto end;
   }
 
-  if (!(dec_ctx = avcodec_alloc_context3(dec))) {
-    fprintf(stderr, "Failed to allocate decoder context.\n");
-    ret = AVERROR(ENOMEM);
+  out_stream = out_ctx->fmt_ctx->streams[0];
+
+  if ((ret = transcode(in_ctx, in_stream, out_ctx, out_stream)) < 0) {
+    fprintf(stderr, "Failed to transcode.\n");
     goto end;
   }
 
-  if ((ret = avcodec_parameters_to_context(dec_ctx, in_stream->codecpar)) < 0) {
-    fprintf(stderr, "Failed to copy codec parameters to decoder context.\n");
+  in_ctx->init_pkt = NULL;
+  if ((ret = decode_packet(in_ctx, in_stream, out_ctx, out_stream)) < 0) {
+    fprintf(stderr, "Failed to flush decoder.\n");
     goto end;
   }
 
-  if ((ret = avcodec_open2(dec_ctx, dec, NULL)) < 0) {
-    fprintf(stderr, "Failed to open decoder.\n");
+  in_ctx->dec_frame = NULL;
+  if ((ret = encode_frame(in_ctx, in_stream, out_ctx, out_stream)) < 0) {
+    fprintf(stderr, "Failed to flush encoder.\n");
     goto end;
   }
 
-  if (!(enc = avcodec_find_encoder_by_name(encoder))) {
-    fprintf(stderr, "Failed to find encoder.\n");
-    ret = AVERROR_UNKNOWN;
-    goto end;
-  }
-
-  if (!(enc_ctx = avcodec_alloc_context3(enc))) {
-    fprintf(stderr, "Failed to allocate encoder context.\n");
-    ret = AVERROR(ENOMEM);
-    goto end;
-  }
-
-  enc_ctx->time_base = in_stream->time_base;
-  enc_ctx->framerate = av_guess_frame_rate(in_fmt_ctx, in_stream, NULL);
-
-  enc_ctx->width = in_stream->codecpar->width;
-  enc_ctx->height = in_stream->codecpar->height;
-  enc_ctx->pix_fmt = in_stream->codecpar->format;
-
-  enc_ctx->color_primaries = in_stream->codecpar->color_primaries;
-  enc_ctx->color_trc = in_stream->codecpar->color_trc;
-  enc_ctx->colorspace = in_stream->codecpar->color_space;
-  enc_ctx->color_range = in_stream->codecpar->color_range;
-  enc_ctx->chroma_sample_location = in_stream->codecpar->chroma_location;
-
-  enc_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
-
-  if (enc_params && enc_params_opt) {
-    if ((ret = av_opt_set(enc_ctx->priv_data,
-      enc_params_opt, enc_params, 0)) < 0)
-    {
-      fprintf(stderr, "Failed to set %s\n", enc_params_opt);
-      goto end;
-    }
-  }
-
-  if ((ret = inject_hdr_metadta(hdr_ctx, enc_ctx)) < 0) {
-    fprintf(stderr, "Failed to inject hdr metadata.\n");
-    goto end;
-  }
-
-  if ((ret = avcodec_open2(enc_ctx, enc, NULL)) < 0) {
-    fprintf(stderr, "Failed to open encoder.\n");
-    goto end;
-  }
-
-  if ((ret = avformat_alloc_output_context2(&out_fmt_ctx, NULL, NULL, out_filename))) {
-    fprintf(stderr, "Failed to allocate output format context.\n");
-    goto end;
-  }
-
-  if ((ret = av_opt_set(out_fmt_ctx, "strict", "unofficial", 0)) < 0) {
-    fprintf(stderr, "Failed to set -strict unofficial.\n");
-    goto end;
-  }
-
-  if ((ret = av_dict_copy(&out_fmt_ctx->metadata,
-    in_fmt_ctx->metadata, AV_DICT_DONT_OVERWRITE)) < 0)
-  {
-    fprintf(stderr, "Failed to copy file metadata.\n");
-    goto end;
-  }
-
-  if (!(out_stream = avformat_new_stream(out_fmt_ctx, NULL))) {
-    fprintf(stderr, "Failed to allocate new output stream.\n");
-    ret = AVERROR(ENOMEM);
-    goto end;
-  }
-
-  if ((ret = av_dict_copy(&out_stream->metadata,
-    in_stream->metadata, AV_DICT_DONT_OVERWRITE)) < 0)
-  {
-    fprintf(stderr, "Failed to copy video metadata.\n");
-    goto end;
-  }
-
-  if ((ret = avcodec_parameters_from_context(out_stream->codecpar, enc_ctx))) {
-    fprintf(stderr,
-      "Failed to copy codec parameters from encoder context to stream.\n");
-    goto end;
-  }
-
-  out_stream->time_base = enc_ctx->time_base;
-  out_stream->r_frame_rate = in_stream->r_frame_rate;
-  out_stream->avg_frame_rate = in_stream->avg_frame_rate;
-
-  if (!(pkt = av_packet_alloc())) {
-    fprintf(stderr, "Failed to allocate AVPacket.\n");
-    ret = AVERROR(ENOMEM);
-    goto end;
-  }
-
-  if (!(frame = av_frame_alloc())) {
-    fprintf(stderr, "Failed to allocate AVFrame.\n");
-    ret = AVERROR(ENOMEM);
-    goto end;
-  }
-
-  if (!(out_fmt_ctx->oformat->flags & AVFMT_NOFILE)) {
-    if ((ret = avio_open(&out_fmt_ctx->pb, out_filename, AVIO_FLAG_WRITE)) < 0) {
-      fprintf(stderr, "Failed to open output file.\n");
-      goto end;
-    }
-  }
-
-  if ((ret = avformat_write_header(out_fmt_ctx, NULL)) < 0) {
-    fprintf(stderr, "Failed to write header for output file.\n");
-    goto end;
-  }
-
-  while ((ret = av_read_frame(in_fmt_ctx, pkt)) >= 0)
-  {
-    if (!(pkt->stream_index == v_stream_idx)) {
-      av_packet_unref(pkt);
-      continue;
-    }
-
-    if ((ret = avcodec_send_packet(dec_ctx, pkt)) < 0) {
-      fprintf(stderr, "Failed to send packet to decoder.\n");
-      goto end;
-    }
-
-    while ((ret = avcodec_receive_frame(dec_ctx, frame)) >= 0)
-    {
-      if ((ret = avcodec_send_frame(enc_ctx, frame)) < 0) {
-        fprintf(stderr, "Failed to send frame to encoder.\n");
-        goto end;
-      }
-
-      while ((ret = avcodec_receive_packet(enc_ctx, pkt)) >= 0)
-      {
-        pkt->stream_index = 0;
-        av_packet_rescale_ts(pkt, in_stream->time_base, out_stream->time_base);
-
-        if ((ret = av_interleaved_write_frame(out_fmt_ctx, pkt)) < 0) {
-          fprintf(stderr, "Failed to write packet to file.\n");
-          goto end;
-        }
-        av_packet_unref(pkt);
-      }
-
-      if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
-        fprintf(stderr, "Failed to receive packet from encoder.\n");
-        goto end;
-      }
-    }
-
-    if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
-      fprintf(stderr, "Failed to receive frame from decoder.\n");
-      goto end;
-    }
-  }
-
-  if ((ret != AVERROR(EAGAIN)) && (ret != AVERROR_EOF)) {
-    fprintf(stderr, "Failed to read frame from input file.\n");
-    goto end;
-  }
-
-  if ((ret = avcodec_send_frame(enc_ctx, NULL)) < 0) {
-    fprintf(stderr, "Failed to send frame to encoder.\n");
-    goto end;
-  }
-
-  if ((ret = av_write_trailer(out_fmt_ctx)) < 0) {
+  if ((ret = av_write_trailer(out_ctx->fmt_ctx)) < 0) {
     fprintf(stderr, "Failed to write trailer to file.\n");
     goto end;
   }
 
 end:
+  close_input(in_ctx);
+  close_output(out_ctx);
   free(hdr_ctx);
-  avformat_close_input(&in_fmt_ctx);
-  if (out_fmt_ctx && !(out_fmt_ctx->flags & AVFMT_NOFILE))
-    avio_closep(&out_fmt_ctx->pb);
-  avformat_free_context(out_fmt_ctx);
-  avcodec_free_context(&dec_ctx);
-  avcodec_free_context(&enc_ctx);
-  av_packet_free(&pkt);
-  av_frame_free(&frame);
 
   if (ret < 0 && ret != AVERROR(EAGAIN) && ret != AVERROR_EOF) {
     fprintf(stderr, "\nLibav Error: %s\n", av_err2str(ret));
