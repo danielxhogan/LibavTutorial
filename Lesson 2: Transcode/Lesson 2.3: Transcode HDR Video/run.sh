da455e761ca52c0071113549ab982f3583945d89
danielxhogan <danielxhogan@gmail.com>
2025-12-13 18:00:19 -0800

    update input videos and run commands

--- Lesson 2: Transcode/Lesson 2.3: Transcode HDR Video/run.sh
+++ Lesson 2: Transcode/Lesson 2.3: Transcode HDR Video/run.sh
@@ -1,13 +1,34 @@
-# cd build && ninja && ./hdrtranscode "../videos/inputs/her_dovi.mkv" "../videos/outputs/her_dovi_svtav1.mkv" "libsvtav1"
+# # heretic has dolbyvision hdr
+# cd build && ninja && ./hdrtranscode "../videos/inputs/heretic.mkv" "../videos/outputs/heretic_dovi_svtav1.mkv" "libsvtav1" "preset=10:crf=30"
 # cd ..
-# cd build && ninja && ./hdrtranscode "../videos/inputs/her_dovi.mkv" "../videos/outputs/her_dovi_x265.mkv" "libx265"
+# cd build && ninja && ./hdrtranscode "../videos/inputs/heretic.mkv" "../videos/outputs/heretic_dovi_x265.mkv" "libx265" "crf=30"
 # cd ..
-# cd build && ninja && ./hdrtranscode "../videos/inputs/her_dovi.mkv" "../videos/outputs/her_dovi_x265.mp4" "libx265"
+# cd build && ninja && ./hdrtranscode "../videos/inputs/heretic.mkv" "../videos/outputs/heretic_dovi_x265.mp4" "libx265" "crf=30"
 # cd ..
-# cd build && ninja && ./hdrtranscode "../videos/inputs/her_dovi.mkv" "../videos/outputs/her_dovi_x265_params.mkv" "libx265" "preset=ultrafast:crf=45"
+
+# # noes has standard hdr10
+# cd build && ninja && ./hdrtranscode "../videos/inputs/noes.mkv" "../videos/outputs/noes_hdr_svtav1.mkv" "libsvtav1" "preset=10:crf=30"
 # cd ..
-# cd build && ninja && ./hdrtranscode "../videos/inputs/ll_hdr.mkv" "../videos/outputs/ll_hdr_svtav1.mkv" "libsvtav1" "preset=9:crf=45"
+# cd build && ninja && ./hdrtranscode "../videos/inputs/noes.mkv" "../videos/outputs/noes_hdr_x265.mkv" "libx265" "crf=30"
 # cd ..
-# cd build && ninja && ./hdrtranscode "../videos/inputs/ll_hdr.mkv" "../videos/outputs/ll_hdr_x265.mkv" "libx265" "preset=ultrafast:crf=45"
+# cd build && ninja && ./hdrtranscode "../videos/inputs/noes.mkv" "../videos/outputs/noes_hdr_x265.mp4" "libx265" "crf=30"
+# cd ..
+
+# # whiplash has standard hdr10
+# cd build && ninja && ./hdrtranscode "../videos/inputs/whiplash.mkv" "../videos/outputs/whiplash_hdr_svtav1.mkv" "libsvtav1" "preset=10:crf=30"
+# cd ..
+# cd build && ninja && ./hdrtranscode "../videos/inputs/whiplash.mkv" "../videos/outputs/whiplash_hdr_x265.mkv" "libx265" "crf=30"
+# cd ..
+# cd build && ninja && ./hdrtranscode "../videos/inputs/whiplash.mkv" "../videos/outputs/whiplash_hdr_x265.mp4" "libx265" "crf=30"
+# cd ..
+
+# ll is standard hd with no hdr
+# cd build && ninja && ./hdrtranscode "../videos/inputs/ll.mkv" "../videos/outputs/ll_sdr_svtav1.mkv" "libsvtav1" "preset=10:crf=30"
+# cd ..
+# cd build && ninja && ./hdrtranscode "../videos/inputs/ll.mkv" "../videos/outputs/ll_sdr_x265.mkv" "libx265" "crf=30"
+# cd ..
+# cd build && ninja && ./hdrtranscode "../videos/inputs/ll.mkv" "../videos/outputs/ll_sdr_x265.mp4" "libx265" "crf=30"
+# cd ..
+
 
 cd build && ninja && ./hdrtranscode 
