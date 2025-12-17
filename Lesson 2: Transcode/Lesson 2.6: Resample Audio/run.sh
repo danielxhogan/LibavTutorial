82f7c2b5ac13a2b0a30cc839148799f63abb2793
danielxhogan <danielxhogan@gmail.com>
2025-12-17 11:48:02 -0800

    update input videos and run commands

--- Lesson 2: Transcode/Lesson 2.6: Resample Audio/run.sh
+++ Lesson 2: Transcode/Lesson 2.6: Resample Audio/run.sh
@@ -2,6 +2,12 @@
 # cd build && ninja && ./swrtranscode "../videos/inputs/noes_ac3_51.mkv" "../videos/outputs/noes_ac3_to_aac_51.mkv" "libfdk_aac"
 # cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_aac_20.mkv" "libfdk_aac"
 
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_ac3_20.mkv" "ac3"
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_mp3_20.mkv" "libmp3lame"
+
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_vorbis_20.mkv" "libvorbis"
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_opus_20.mkv" "libvopus"
+
 # cd build && ninja && ./swrtranscode "../videos/inputs/ll_hdma_51.mkv" "../videos/outputs/ll_hdma_to_aac_51.mkv" "libfdk_aac"
 # cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_51.mkv" "../videos/outputs/ll_dts_to_aac_51.mkv" "libfdk_aac"
 # cd build && ninja && ./swrtranscode "../videos/inputs/noes_hdma_20.mkv" "../videos/outputs/noes_hdma_to_aac_20.mkv" "libfdk_aac"
