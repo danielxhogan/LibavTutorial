f067d61a8ad738568011c470debe961df91f7e68
danielxhogan <danielxhogan@gmail.com>
2025-12-17 20:16:28 -0800

    add better test coverage to Lesson 2.6

--- Lesson 2: Transcode/Lesson 2.6: Resample Audio/run.sh
+++ Lesson 2: Transcode/Lesson 2.6: Resample Audio/run.sh
@@ -1,16 +1,142 @@
-# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_truehd_71.mkv" "../videos/outputs/heretic_truehd_to_aac_71.mkv" "libfdk_aac"
-# cd build && ninja && ./swrtranscode "../videos/inputs/noes_ac3_51.mkv" "../videos/outputs/noes_ac3_to_aac_51.mkv" "libfdk_aac"
+# # transcode to aac
+# # ****************
+# # No issues
 # cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_aac_20.mkv" "libfdk_aac"
-
-# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_ac3_20.mkv" "ac3"
-# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_mp3_20.mkv" "libmp3lame"
-
-# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_vorbis_20.mkv" "libvorbis"
-# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_opus_20.mkv" "libvopus"
-
-# cd build && ninja && ./swrtranscode "../videos/inputs/ll_hdma_51.mkv" "../videos/outputs/ll_hdma_to_aac_51.mkv" "libfdk_aac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_51.mkv" "../videos/outputs/heretic_ac3_to_aac_51.mkv" "libfdk_aac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_truehd_71.mkv" "../videos/outputs/heretic_truehd_to_aac_71.mkv" "libfdk_aac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_aac_20.mkv" "libfdk_aac"
+# cd ..
 # cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_51.mkv" "../videos/outputs/ll_dts_to_aac_51.mkv" "libfdk_aac"
-# cd build && ninja && ./swrtranscode "../videos/inputs/noes_hdma_20.mkv" "../videos/outputs/noes_hdma_to_aac_20.mkv" "libfdk_aac"
+# cd ..
 # cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_20.mkv" "../videos/outputs/ll_dts_to_aac_20.mkv" "libfdk_aac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/noes_hdma_20.mkv" "../videos/outputs/noes_hdma_to_aac_20.mkv" "libfdk_aac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/whiplash_hdma_51.mkv" "../videos/outputs/whiplash_hdma_to_aac_51.mkv" "libfdk_aac"
+# cd ..
+
+# # transcode to flac
+# # *******************
+# # No issues
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_flac_20.mkv" "flac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_51.mkv" "../videos/outputs/heretic_ac3_to_flac_51.mkv" "flac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_truehd_71.mkv" "../videos/outputs/heretic_truehd_to_flac_71.mkv" "flac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_flac_20.mkv" "flac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_51.mkv" "../videos/outputs/ll_dts_to_flac_51.mkv" "flac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_20.mkv" "../videos/outputs/ll_dts_to_flac_20.mkv" "flac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/noes_hdma_20.mkv" "../videos/outputs/noes_hdma_to_flac_20.mkv" "flac"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/whiplash_hdma_51.mkv" "../videos/outputs/whiplash_hdma_to_flac_51.mkv" "flac"
+# cd ..
+
+# # transcode to opus
+# # *****************
+# # No issues
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_opus_20.mkv" "libopus"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_51.mkv" "../videos/outputs/heretic_ac3_to_opus_51.mkv" "libopus"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_truehd_71.mkv" "../videos/outputs/heretic_truehd_to_opus_71.mkv" "libopus"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_opus_20.mkv" "libopus"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_51.mkv" "../videos/outputs/ll_dts_to_opus_51.mkv" "libopus"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_20.mkv" "../videos/outputs/ll_dts_to_opus_20.mkv" "libopus"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/noes_hdma_20.mkv" "../videos/outputs/noes_hdma_to_opus_20.mkv" "libopus"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/whiplash_hdma_51.mkv" "../videos/outputs/whiplash_hdma_to_opus_51.mkv" "libopus"
+# cd ..
+
+# # transcode to eac3
+# # *****************
+# # Transcoding from Truehd or AAC results in clicks & pops
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_eac3_20.mkv" "eac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_51.mkv" "../videos/outputs/heretic_ac3_to_eac3_51.mkv" "eac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_truehd_71.mkv" "../videos/outputs/heretic_truehd_to_eac3_71.mkv" "eac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_eac3_20.mkv" "eac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_51.mkv" "../videos/outputs/ll_dts_to_eac3_51.mkv" "eac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_20.mkv" "../videos/outputs/ll_dts_to_eac3_20.mkv" "eac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/noes_hdma_20.mkv" "../videos/outputs/noes_hdma_to_eac3_20.mkv" "eac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/whiplash_hdma_51.mkv" "../videos/outputs/whiplash_hdma_to_eac3_51.mkv" "eac3"
+# cd ..
+
+# # transcode to ac3
+# # ****************
+# # Transcoding from Truehd or AAC results in clicks & pops
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_ac3_20.mkv" "ac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_51.mkv" "../videos/outputs/heretic_ac3_to_ac3_51.mkv" "ac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_truehd_71.mkv" "../videos/outputs/heretic_truehd_to_ac3_71.mkv" "ac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_ac3_20.mkv" "ac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_51.mkv" "../videos/outputs/ll_dts_to_ac3_51.mkv" "ac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_20.mkv" "../videos/outputs/ll_dts_to_ac3_20.mkv" "ac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/noes_hdma_20.mkv" "../videos/outputs/noes_hdma_to_ac3_20.mkv" "ac3"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/whiplash_hdma_51.mkv" "../videos/outputs/whiplash_hdma_to_ac3_51.mkv" "ac3"
+# cd ..
+
+# # transcode to vorbis
+# # *******************
+# # Transcoding all videos results in distorted audio.
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_vorbis_20.mkv" "libvorbis"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_51.mkv" "../videos/outputs/heretic_ac3_to_vorbis_51.mkv" "libvorbis"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_truehd_71.mkv" "../videos/outputs/heretic_truehd_to_vorbis_71.mkv" "libvorbis"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_vorbis_20.mkv" "libvorbis"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_51.mkv" "../videos/outputs/ll_dts_to_vorbis_51.mkv" "libvorbis"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_20.mkv" "../videos/outputs/ll_dts_to_vorbis_20.mkv" "libvorbis"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/noes_hdma_20.mkv" "../videos/outputs/noes_hdma_to_vorbis_20.mkv" "libvorbis"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/whiplash_hdma_51.mkv" "../videos/outputs/whiplash_hdma_to_vorbis_51.mkv" "libvorbis"
+# cd ..
+
+# # transcode to mp3
+# # ****************
+# # Transcoding all videos results in distorted audio.
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_20.mkv" "../videos/outputs/heretic_ac3_to_mp3_20.mkv" "libmp3lame"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_ac3_51.mkv" "../videos/outputs/heretic_ac3_to_mp3_51.mkv" "libmp3lame"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/heretic_truehd_71.mkv" "../videos/outputs/heretic_truehd_to_mp3_71.mkv" "libmp3lame"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_aac_20.mkv" "../videos/outputs/ll_aac_to_mp3_20.mkv" "libmp3lame"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_51.mkv" "../videos/outputs/ll_dts_to_mp3_51.mkv" "libmp3lame"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/ll_dts_20.mkv" "../videos/outputs/ll_dts_to_mp3_20.mkv" "libmp3lame"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/noes_hdma_20.mkv" "../videos/outputs/noes_hdma_to_mp3_20.mkv" "libmp3lame"
+# cd ..
+# cd build && ninja && ./swrtranscode "../videos/inputs/whiplash_hdma_51.mkv" "../videos/outputs/whiplash_hdma_to_mp3_51.mkv" "libmp3lame"
+# cd ..
+
 
 # cd build && ninja && ./swrtranscode
