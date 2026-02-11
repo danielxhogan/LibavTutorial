e6c44bd30b97880fc87f977db4686890d5f5edd7
danielxhogan <danielxhogan@gmail.com>
2026-02-11 15:02:07 -0800

    udpate filter

--- Lesson 3: Filter/Lesson 3.1: Filter Video 1 Input 1 Output/run.sh
+++ Lesson 3: Filter/Lesson 3.1: Filter Video 1 Input 1 Output/run.sh
@@ -1,3 +1,3 @@
-# cd build && ninja && ./fv1in1out "../videos/inputs/bttf.mkv" "../videos/outputs/bttf_deinterlaced.mkv" "yadif" "libx265" "crf=10"
+# cd build && ninja && ./fv1in1out "../videos/inputs/bttf.mkv" "../videos/outputs/bttf_deinterlaced.mkv" "libplacebo=deinterlace=yadif" "libx265" "crf=10"
 
 cd build && ninja && ./fv1in1out 
